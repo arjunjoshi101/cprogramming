e7920fbd15b78cb1b986c3f2131583503860661b
arjun joshi <arjun94@gmail.com>
2026-02-23 16:15:23 +0000

    committing 2 files

---
+++ second.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main () {
+
+printf("hello world 2\n");
+
+
+    return 0;
+}
---
+++ third.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main () {
+
+printf("hello world 3\n");
+
+
+    return 0;
+}
