4e1a95de609e20be07f54566882b940d0eb3be6f
arjun joshi <arjun94@gmail.com>
2026-02-25 15:28:36 +0000

    first change by arjun

--- third.c
+++ third.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main () {
+int main()
+{
 
-printf("hello world 3\n");
-
-
+    printf("hello world 3\n");
+    printf("changed by arjun on 25th feb 2026\n");
     return 0;
 }
